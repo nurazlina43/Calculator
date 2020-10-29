6ff6056a68911f2d97fa9cca99ce486b03e368d5
nurazlina43 <72524663+nurazlina43@users.noreply.github.com>
2020-10-30 03:04:52 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+    <heading />
+    <calculator />
+</div>
+</template>
+
+<script>
+import heading from './components/heading.vue'
+import calculator from './components/calculator.vue'
+export default {
+    name: 'App',
+    components: {
+        heading,
+        calculator
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 30px;
+}
+</style>
---
+++ src/components/heading.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Calculator Application"
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: #fff;
+}
+</style>
